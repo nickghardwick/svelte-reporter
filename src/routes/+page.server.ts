392b6788cebef25a8ba7627d11459670b9a49455
Nick Hardwick <nickghardwick@gmail.com>
2023-01-11 21:59:24 -0800

    feat: wire up server-side reqeusts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,47 @@
-import type { Actions } from './$types';
+import axios from "axios";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone.js";
+import utc from "dayjs/plugin/utc.js";
+import type { Actions } from "./$types";
+
+dayjs.extend(timezone);
+dayjs.extend(utc);
 
 export const actions: Actions = {
-	default: async ({ request }) => {
-		const data = await request.formData();
-		console.log(data);
+  default: async ({ request }) => {
+    const data = await request.formData();
+    const expenseData = {
+      Timestamp: dayjs().tz("America/Los_Angeles").format("MM/DD/YYYYTHH:mm:ss"),
+      Expense: data.get("expense"),
+      Amount: data.get("amount"),
+      Category: data.get("category"),
+    };
 
-		return { success: true };
-	},
+    const sheetDBReq = axios.post(`${import.meta.env.VITE_SHEETDB_URL}`, {
+      data: [expenseData],
+      sheet: "Ledger",
+    });
+
+    const supabaseReq = axios.post(
+      "https://okvzazvzpoxaztttoshl.supabase.co/rest/v1/2023",
+      {
+        Expense: expenseData.Expense,
+        Amount: expenseData.Amount,
+        Category: expenseData.Category,
+      },
+      {
+        headers: {
+          apikey: `${import.meta.env.VITE_SUPABASE_KEY}`,
+          Authorization: `Bearer ${import.meta.env.VITE_SUPABASE_KEY}`,
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    const [sheetDBRes, supabaseRes] = await Promise.all([sheetDBReq, supabaseReq]);
+
+    console.log(sheetDBRes.data, supabaseRes.data);
+
+    return { success: true };
+  },
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+  import type { ActionData } from "./$types";
+
+  export let form: ActionData;
+</script>
+
 <div class="p-4">
   <h1 class="text-4xl font-bold text-center">Expense Reporter</h1>
   <div class="pt-4">
@@ -12,6 +18,7 @@
             type="text"
             placeholder="Type here"
             class="input input-bordered w-full max-w-xs"
+            required
           />
         </div>
         <div class="form-control w-full max-w-xs self-center">
@@ -23,13 +30,14 @@
             type="text"
             placeholder="Type here"
             class="input input-bordered w-full max-w-xs"
+            required
           />
         </div>
         <div class="form-control w-full max-w-xs self-center">
           <label for="category" class="label">
             <span class="label-text">Category <span class="text-red-600 font-bold">*</span></span>
           </label>
-          <select name="category" class="select select-bordered">
+          <select name="category" class="select select-bordered" required>
             <option disabled selected hidden>Pick one</option>
             <option>Coffee</option>
             <option>Eating Out</option>
@@ -43,9 +51,18 @@
           </select>
         </div>
         <div class="w-auto">
-          <button class="btn btn-success btn-md">Success</button>
+          <button class="btn btn-success btn-md">Submit</button>
         </div>
       </div>
     </form>
   </div>
+  {#if form?.success}
+    <div class="toast toast-end">
+      <div class="alert alert-success">
+        <div>
+          <span>Expense submitted</span>
+        </div>
+      </div>
+    </div>
+  {/if}
 </div>
